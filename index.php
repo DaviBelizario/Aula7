82222af43a8b79003c068d648c46b21e40171c09
Davi Belizario <NinjaBS.stormU01@outlook.com.br>
2024-11-13 19:41:40 -0300

    Davi

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Loja de Moedas de Jogos</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+
+    <h1>Loja de Moedas de Jogos</h1>
+    <h2> Encontre as melhores moedas virtuais para todos os seus jogos favoritos em um só lugar. Temos uma seleção incrível de moedas para PC, consoles e dispositivos móveis, abrangendo todos os estilos de jogos: ação, aventura, estratégia e muito mais! Nosso objetivo é oferecer a moeda perfeita para elevar sua experiência de jogo, com ofertas atualizadas regularmente.</h2>
+    <h2> Descubra o Novo e o Clássico em um Só Lugar. Nossas moedas são cuidadosamente selecionadas para garantir qualidade e satisfação. Fique por dentro das ofertas mais recentes, explore opções de moedas favoritas de todos os tempos e aproveite recomendações personalizadas de acordo com suas preferências.</h2>
+    <br/><br/><br/><br/><br/>
+    <div class="loja">
+        <h1>Menu de Operações</h1>
+        <button id="cadastroBtn">Cadastro</button><br/><br/>
+        <button id="vendaBtn">Venda</button>
+    </div>
+    <script type="text/javascript" src="js/menu.js"></script>
+</body>
+</html>
